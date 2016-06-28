86b2f2391e3b12ed1be521d340447b68a69bc141
Avi Y <aviyaakov1@gmail.com>
2016-06-28 12:24:01 +0300

    now emmiting the message

--- socket.config.js
+++ socket.config.js
@@ -1,9 +1,10 @@
 var socket = function (io, User) {
 	io.on('connection', function (socket) {
 		socket.on('message', function (data) {
+			var fromUser;
 			var filter = {
 				email: data.from
-			}
+			};
 			User.findOne(filter, function (err, curentUser) {
 				for (var i = 0; i < curentUser.chats.length; i++) {
 					if (curentUser.chats[i].user.email === data.to) {
@@ -14,6 +15,7 @@ var socket = function (io, User) {
 						break;
 					}
 				}
+				fromUser = curentUser;
 	        	curentUser.save(function (err) {
 	        		//TODO
 	        	});
@@ -23,7 +25,8 @@ var socket = function (io, User) {
 				email: data.to
 			};
 			User.findOne(filter, function (err, toUser) {
-				for (var i = 0; i < toUser.chats.length; i++) {
+				var i = 0
+				for (i ; i < toUser.chats.length; i++) {
 					if (toUser.chats[i].user.email === data.from) {
 						toUser.chats[i].messages.push({
 							from: data.from,
@@ -32,11 +35,27 @@ var socket = function (io, User) {
 						break;
 					}
 				}
+				if (i === toUser.chats.length) {
+					fromUser.password = undefined;
+					toUser.chats.push({
+		                user: fromUser,
+		                messages: [{
+							from: data.from,
+							message: data.msg
+						}]
+	                });
+				}
 	        	toUser.save(function (err) {
 	        		//TODO
 	        	});
 			});
-			socket.broadcast.to(data.to).emit('message received');
+			socket.broadcast.to(data.to).emit('message received', {
+				msg: {
+					from: data.from,
+					message: data.msg,
+					time: Date.now()
+				}
+			});
 	  	});
 	  	socket.on('get room', function (data) {
 	  		socket.join(data.room);
